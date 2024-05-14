7680832ae3d24a60844d5bbe98fb03829d7efae1
kamna nepal <kamna.npl3343@gmail.com>
2024-05-14 19:16:43 +0545

    Add .NET file

--- HotelAssignment/obj/Release/net8.0/HotelAssignment.AssemblyInfo.cs
+++ HotelAssignment/obj/Release/net8.0/HotelAssignment.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelAssignment")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0682af0caae7ad95afa228fd6b6adc58899598")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelAssignment")]
